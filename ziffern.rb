6b60d651e1490dfdde30b87875cc4f7e03e87730
Jimmy Börjesson <lagginglion@gmail.com>
2014-08-05 18:41:37 +0200

    Moved 'Komma' to the beginning

--- ziffern.rb
+++ ziffern.rb
@@ -35,10 +35,11 @@ class Ziffern
     decimals = number.to_s[/\.(\d+)/, 1]
     return '' if decimals.nil?
 
-    decimals
-      .chars.map(&:to_i)
-      .map { |digit| to_german(digit) }
-      .join(' ').prepend(' Komma ')
+    ' Komma ' +
+      decimals
+        .chars.map(&:to_i)
+        .map { |digit| to_german(digit) }
+        .join(' ')
   end
 
   def twenty_to_99(number)
