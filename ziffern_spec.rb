1bdc43d801a42ffa5893c1cc81b694d0c6ff3b24
Jimmy Börjesson <lagginglion@gmail.com>
2015-08-25 19:19:57 +0200

    Remove unnecessary curly braces

--- ziffern_spec.rb
+++ ziffern_spec.rb
@@ -15,34 +15,34 @@ describe Ziffern do
   end
 
   context 'numbers up to 20' do
-    test_german_numbers({
+    test_german_numbers(
       0  => "null",
       1  => "eins",
       13 => "dreizehn",
       19 => "neunzehn",
-    })
+    )
   end
 
   context 'numbers up to 100' do
-    test_german_numbers({
+    test_german_numbers(
       20 => "zwanzig",
       21 => "einundzwanzig",
       63 => "dreiundsechzig",
       99 => "neunundneunzig",
-    })
+    )
   end
 
   context 'numbers up to 1000' do
-    test_german_numbers({
+    test_german_numbers(
       100 => "einhundert",
       101 => "einhunderteins",
       234 => "zweihundertvierunddreißig",
       999 => "neunhundertneunundneunzig",
-    })
+    )
   end
 
   context 'numbers up to one million' do
-    test_german_numbers({
+    test_german_numbers(
       1000    => "eintausend",
       1001    => "eintausendeins",
       101000  => "einhunderteintausend",
@@ -50,48 +50,48 @@ describe Ziffern do
       1234    => "eintausendzweihundertvierunddreißig",
       10_000  => "zehntausend",
       999_999 => "neunhundertneunundneunzigtausendneunhundertneunundneunzig",
-    })
+    )
   end
 
   context 'really big numbers' do
-    test_german_numbers({
+    test_german_numbers(
       1_000_000     => "eine Million",
       1_000_001     => "eine Million eins",
       2_204_510     => "zwei Millionen zweihundertviertausendfünfhundertzehn",
       1_203_400_021 => "eine Milliarde zweihundertdrei Millionen vierhunderttausendeinundzwanzig",
       3_000_000_000_000_099 => "drei Billiarden neunundneunzig",
       10 ** 123 => "eine Vigintilliarde"
-    })
+    )
   end
 
   context 'negative numbers' do
-    test_german_numbers({
+    test_german_numbers(
       -1  => "minus eins",
       -20 => "minus zwanzig",
       -21 => "minus einundzwanzig",
       -63 => "minus dreiundsechzig",
       -99 => "minus neunundneunzig",
-    })
+    )
   end
 
   context 'decimals' do
-    test_german_numbers({
+    test_german_numbers(
       0.1234  => "null Komma eins zwei drei vier",
       5.6789  => "fünf Komma sechs sieben acht neun",
       5.0     => "fünf Komma null",
       -5.6    => "minus fünf Komma sechs",
       -567.89 => "minus fünfhundertsiebenundsechzig Komma acht neun",
       -0.001  => "minus null Komma null null eins"
-    })
+    )
   end
 
   context 'strings' do
-    test_german_numbers({
+    test_german_numbers(
       '-0'     => "null",
       '-123'   => "minus einhundertdreiundzwanzig",
       '-123.1' => "minus einhundertdreiundzwanzig Komma eins",
       '5.00'   => "fünf Komma null null",
-    })
+    )
   end
 
   context 'errors' do
